31d9d7edfe060adc910f3c2b0b9794b01c8710bb
kcountryman <35474352+kcountryman@users.noreply.github.com>
2018-02-01 09:00:37 -0600

    Add files via upload

---
+++ HW 2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 08:56:42 2018
+
+@author: kcoun
+"""
+
+a=[1,3,5,7,9]
+for loopElem in a:
+    print(loopElem)
+    
+
+myMap={0:"apples",1:"bananas",2:"oranges"}
+print(myMap)
+
+for loopVar in myMap:
+    print(myMap[loopVar])
